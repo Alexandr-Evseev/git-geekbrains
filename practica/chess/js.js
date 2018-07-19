2b49b4e24eedb815da938457ab0a8f041913c231
Alexandr-Evseev <alhimicom@gmail.com>
2018-07-20 02:59:11 +0500

    Расставил и отобразил фигуры на доске.

--- practica/chess/js.js
+++ practica/chess/js.js
@@ -3,7 +3,56 @@
 
 const chess = {
   gameContainerEl: document.getElementById('game'),
+  figures: [
+    {name:'p', color:'w', pos:'a2'},
+    {name:'p', color:'w', pos:'b2'},
+    {name:'p', color:'w', pos:'c2'},
+    {name:'p', color:'w', pos:'d2'},
+    {name:'p', color:'w', pos:'e2'},
+    {name:'p', color:'w', pos:'f2'},
+    {name:'p', color:'w', pos:'g2'},
+    {name:'p', color:'w', pos:'h2'},
+    {name:'R', color:'w', pos:'a1'},
+    {name:'N', color:'w', pos:'b1'},
+    {name:'B', color:'w', pos:'c1'},
+    {name:'Q', color:'w', pos:'d1'},
+    {name:'K', color:'w', pos:'e1'},
+    {name:'B', color:'w', pos:'f1'},
+    {name:'N', color:'w', pos:'g1'},
+    {name:'R', color:'w', pos:'h1'},
 
+    {name:'p', color:'b', pos:'a7'},
+    {name:'p', color:'b', pos:'b7'},
+    {name:'p', color:'b', pos:'c7'},
+    {name:'p', color:'b', pos:'d7'},
+    {name:'p', color:'b', pos:'e7'},
+    {name:'p', color:'b', pos:'f7'},
+    {name:'p', color:'b', pos:'g7'},
+    {name:'p', color:'b', pos:'h7'},
+    {name:'R', color:'b', pos:'a8'},
+    {name:'N', color:'b', pos:'b8'},
+    {name:'B', color:'b', pos:'c8'},
+    {name:'Q', color:'b', pos:'d8'},
+    {name:'K', color:'b', pos:'e8'},
+    {name:'B', color:'b', pos:'f8'},
+    {name:'N', color:'b', pos:'g8'},
+    {name:'R', color:'b', pos:'h8'},
+  ],
+  figureHtml: {
+    pw: '&#9817',
+    Bw: '&#9815',
+    Nw: '&#9816',
+    Rw: '&#9814',
+    Qw: '&#9813',
+    Kw: '&#9818',
+
+    pb: '&#9823',
+    Bb: '&#9821',
+    Nb: '&#9822',
+    Rb: '&#9820',
+    Qb: '&#9819',
+    Kb: '&#9818',
+  },
   renderMap() {
     const rows = [0, 8, 7, 6, 5, 4, 3, 2, 1, 0];
     const cols = [0, 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 0];
@@ -20,6 +69,9 @@ const chess = {
           td.innerHTML = cols[col];
         } else if (cols[col] === 0 && rows[row] !== 0) {
           td.innerHTML = rows[row];
+        } else  if (cols[col] !== 0 || rows[row] !== 0) {
+          td.dataset.row = rows[row];
+          td.dataset.col = cols[col];
         }
 
         if (this.isCellIsBlack(row, col)) {
@@ -41,6 +93,19 @@ const chess = {
     }
     return (rowNum % 2 === 0 && colNum % 2 === 1) || (rowNum % 2 === 1 && colNum % 2 === 0);
   },
+  /**
+   * Отображает фигуры на доске.
+   */
+  renderFigures() {
+    for (const figure of this.figures) {
+      const col = figure.pos.charAt(0);
+      const row = figure.pos.charAt(1);
+
+      document.querySelector(`[data-col='${col}'][data-row='${row}']`).innerHTML =
+        this.figureHtml[figure.name + figure.color];
+    }
+},
 };
 
 chess.renderMap();
+chess.renderFigures();
